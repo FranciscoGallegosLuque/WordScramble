0b817293d5a0e1ded3bd08ba107c32ec08bb55e0
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-03-03 16:01:14 -0300

    [Feature] Accesibility fix

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -76,6 +76,8 @@ struct ContentView: View {
                                 Image(systemName: "\(word.count).circle")
                                 Text(word)
                             }
+                            .accessibilityElement()
+                            .accessibilityLabel("\(word), \(word.count) letters")
                         }
                     }
                 }
